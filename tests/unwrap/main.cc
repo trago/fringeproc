105ff411e2b6b749bc9c0a65884279d2df78ccef
Julio C Estrada <jucesri@gmail.com>
2012-06-19 10:47:37 -0500

    Configuracion de CMake.
    - Se agregaron los archivos de phase unwrapping a la libreria imcore
    - Se quitaron los archivos de phase unwraping del programa unwrap
    - Se corrigio la directiva #include del archivo main.cc del programa
      unwrap

--- tests/unwrap/main.cc
+++ tests/unwrap/main.cc
@@ -2,7 +2,7 @@
 #include <utils/utils.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "unwrap.h"
+#include <imcore/unwrap.h>
 #include <iostream>
 #include <cstdlib>
 
