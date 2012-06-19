281ab1e37b4a31d5a4bba56a050ee29c267c4244
Julio C Estrada <jucesri@gmail.com>
2012-06-19 13:29:35 -0500

    Documenting python classes.

--- tests/unwrap/unwrapimage.py
+++ tests/unwrap/unwrapimage.py
@@ -2,13 +2,37 @@ from PySide.QtGui import QImage
 import numpy as np
 
 class UnwrapImage(QImage):
+  """
+  QImage that loads data from a numpy array.
+  
+  Use this class to create a QImage from a numpy array using the constructor.
+  It only works for gray scale images.
+  
+  Author Julio C. Estrada
+  """
   def __init__(self, np_array):
+    """
+    UnwrapImage(numpy_array).
+    Creates a QImage from the given numpay array.
+    
+    Parameters:
+     - np_array is the numpy array having the data.
+     
+    Author: Julio C. Estrada.
+    """
     self.array = np_array.copy()
     self.mdata = np_array
     self._data = None
     self._fromC1(self.array)
     
   def _fromC1(self, array):
+    """
+    _fromC1(array)
+    Creates the QImage using an array representing gray scaled values.
+    
+    Parameters:
+     - array, is the array data
+    """
     if(array.dtype == np.float32 or array.dtype == np.float64):
       a = array.min()
       b = array.max()
--- tests/unwrap/unwrappixmapitem.py
+++ tests/unwrap/unwrappixmapitem.py
@@ -3,16 +3,55 @@ from PySide.QtCore import QEvent
 from PySide.QtCore import Qt
 
 class UnwrapPixmapItem(QGraphicsPixmapItem):
+  """
+  Pixmap item used to display an image into a graphics scene.
+  
+  Here one can set the callback function that handles the event when the mouse
+  passes over the image.
+  
+  Author: Julio C. Estrada
+  """
+  # This is the event handler function.
+  _moveEventHandler = None  
+  
   def __init__(self, pixmap, parent=None):
+    """
+    UnwrapPixmapItem(pixmap, parent=None)
+    Constructur the pixmap item and activates the event handling when the mouse
+    passes over the image.
+    
+    Author: Julio C. Estrada
+    """
     super(UnwrapPixmapItem, self).__init__(pixmap, parent)
     self.setAcceptHoverEvents(True)
     self.setCursor(Qt.CrossCursor)
     
-    self._moveEventhandler = None
     
   def hoverMoveEvent(self, event):
-    if(self._moveEventhandler is not None):
-      self._moveEventhandler(event.pos())
+    """
+    hoverMoveEvent(event)
+    Called when mouse passes over the image.
+    
+    Parameters:
+     - event, is the object data of the mouse event.
+     
+    Author: Julio C. Estrada
+    """
+    if(self._moveEventHandler is not None):
+      self._moveEventHandler(event.pos())
     
   def setMoveEventHandler(self, handler):
-    self._moveEventhandler = handler
+    """
+    setMoveEventHandler(handler)
+    Sets the handler for the event when the mouse passes over the image.
+    
+    Parameters:
+     - handler, is the reference to the callback function defined as
+       
+       def callback(pos):
+         ...
+         
+    Author: Julio C. Estrada
+      
+    """
+    self._moveEventHandler = handler
