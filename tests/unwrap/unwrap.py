01fc81542e4c3f8f1c4c30bec5d3f8cc1d0dc29a
Julio C. Estrada <jucesri@gmail.com>
2012-07-13 14:32:28 -0500

    Empezando a programar phase-unwrapping en python.
    - Se removió código que lanza la interfaz grafica.

--- tests/unwrap/unwrap.py
+++ tests/unwrap/unwrap.py
@@ -1,21 +1,5 @@
-import sip
-api2_classes = [
-    'QData', 'QDateTime', 'QString', 'QTextStream',
-    'QTime', 'QUrl', 'QVariant']
-for cl in api2_classes:
-    sip.setapi(cl, 2)
-
-import sys
-from PyQt4 import QtGui
-from unwrapgui import UnwrapGUI
-
-def main(argv):
-    app = QtGui.QApplication(argv)
-    gui = UnwrapGUI()
-    gui.show()
-
-    return app.exec_()
+import numpy as np
+import fringeproc as fproc
+import cv2
 
 
-if __name__ == "__main__":
-    main(sys.argv)
